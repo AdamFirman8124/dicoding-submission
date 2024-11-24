b02a7f0e7ed060da291cd3a984412fd1db8884f6
Masoyi <111947405+AdamFirman8124@users.noreply.github.com>
2024-11-24 23:57:29 +0700

    Update handler.js

--- src/server/handler.js
+++ src/server/handler.js
@@ -3,29 +3,44 @@ const storeData = require('../services/storeData');
 const crypto = require('crypto');
 
 async function postPredictHandler(request, h) {
-    const { image } = request.payload;
-    const { model } = request.server.app;
+    try {
+        const { image } = request.payload;
+        const { model } = request.server.app;
 
-    const { confidenceScore, label, explanation, suggestion } = await predictClassification(model, image);
-    const id = crypto.randomUUID();
-    const createdAt = new Date().toISOString();
-    
-    const data = {
-        "id": id,
-        "result": label,
-        "suggestion": suggestion,
-        "createdAt": createdAt
+        // Prediksi dengan memanggil fungsi 'predictClassification'
+        const { confidenceScore, label, explanation, suggestion } = await predictClassification(model, image);
+
+        const id = crypto.randomUUID();
+        const createdAt = new Date().toISOString();
+
+        const data = {
+            "id": id,
+            "result": label,
+            "suggestion": suggestion,
+            "createdAt": createdAt
+        };
+
+        // Simpan data prediksi
+        await storeData(id, data);
+
+        const response = h.response({
+            status: 'success',
+            message: 'Model is predicted successfully.',
+            data
+        });
+        response.code(201);
+        return response;
+    } catch (error) {
+        console.error(error); // Log error untuk debugging
+
+        // Mengembalikan respons error jika ada kesalahan
+        const response = h.response({
+            status: 'fail',
+            message: 'Terjadi kesalahan dalam melakukan prediksi'
+        });
+        response.code(400); // Status code 400 untuk Bad Request
+        return response;
     }
-    await storeData(id, data);
-
-    const response = h.response({
-        status: 'success',
-        message: 'Model is predicted successfully.',
-        data
-    })
-    response.code(201);
-    return response;
-
 }
 
 module.exports = postPredictHandler;

e7ebcb6ebe07abac004997c1f92d51c57525da3d
Masoyi <111947405+AdamFirman8124@users.noreply.github.com>
2024-11-25 01:40:03 +0700

    fix

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
- 
+
 const Hapi = require('@hapi/hapi');
 const routes = require('../server/routes');
 const loadModel = require('../services/loadModel');
@@ -12,45 +12,45 @@ const Inert = require('@hapi/inert');
         host: '0.0.0.0',
         routes: {
             cors: {
-              origin: ['*'],
+                origin: ['*'],
             },
-            }
-             });
- 
-             await server.register(Inert)
+        }
+    });
+
+    await server.register(Inert)
     const model = await loadModel();
     server.app.model = model;
- 
+
     server.route(routes);
-   server.ext('onPreResponse', function (request, h) {
-    const response = request.response;
-    
-    // Check for Payload Too Large error (413)
-    if (response.isBoom && response.output.statusCode === 413) {
-        const newResponse = h.response({
-            status: 'fail',
-            message: 'Payload content length is greater than maximum allowed (1MB).'
-        }).code(413);
-        return newResponse;
-    }
+    server.ext('onPreResponse', function (request, h) {
+        const response = request.response;
 
-     // Jika error terkait prediksi (misalnya InputError atau kesalahan umum)
-    if (response instanceof InputError || response.isBoom) {
-        console.log(response)
-        const message = response instanceof InputError
-            ? `${response.message} Silakan gunakan foto lain.`
-            : 'Terjadi kesalahan dalam melakukan prediksi';
-        
-        const newResponse = h.response({
-            status: 'fail',
-            message: message
-        }).code(400);
-        return newResponse;
-    }
-    return h.continue;
-});
+        // Check for Payload Too Large error (413)
+        if (response.isBoom && response.output.statusCode === 413) {
+            const newResponse = h.response({
+                status: 'fail',
+                message: 'Payload content length is greater than maximum allowed (1MB).'
+            }).code(413);
+            return newResponse;
+        }
 
- 
+        // Jika error terkait prediksi (misalnya InputError atau kesalahan umum)
+        if (response instanceof InputError || response.isBoom) {
+            console.log(response)
+            const message = response instanceof InputError
+                ? `${response.message} Silakan gunakan foto lain.`
+                : 'Terjadi kesalahan dalam melakukan prediksi 1';
+
+            const newResponse = h.response({
+                status: 'fail',
+                message: message
+            }).code(400);
+            return newResponse;
+        }
+        return h.continue;
+    });
+
+
     await server.start();
     console.log(`Server start at: ${server.info.uri}`);
 })();

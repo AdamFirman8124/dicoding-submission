26f3eb2e003ae9c389fb377306956be632d24fbf
Masoyi <111947405+AdamFirman8124@users.noreply.github.com>
2024-11-24 16:37:26 +0700

    Update handler.js

--- src/server/handler.js
+++ src/server/handler.js
@@ -2,10 +2,10 @@ const predictClassification = require('../services/inferenceService');
 const crypto = require('crypto');
 
 async function postPredictHandler(request, h) {
-    // const { image } = request.payload;
-    // const { model } = request.server.app;
+    const { image } = request.payload;
+    const { model } = request.server.app;
 
-    // const { confidenceScore, label, explanation, suggestion } = await predictClassification(model, image);
+    const { confidenceScore, label, explanation, suggestion } = await predictClassification(model, image);
     // const id = crypto.randomUUID();
     // const createdAt = new Date().toISOString();
     // const data = {

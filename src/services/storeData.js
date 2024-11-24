35d2b28cc09ef45b8f2ca048ebe6f7c150e34421
Masoyi <111947405+AdamFirman8124@users.noreply.github.com>
2024-11-25 01:03:15 +0700

    Update storeData.js

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -8,16 +8,6 @@ async function storeData(id, data) {
     console.log('Data stored successfully');
   } catch (error) {
     console.error('Error storing data:', error);
-    console.error(error); // Log error untuk debugging
-
-        // Mengembalikan respons error jika ada kesalahan
-        const response = h.response({
-            status: 'fail',
-            message: 'Terjadi kesalahan dalam melakukan prediksi',
-            error:error,
-        });
-        response.code(400); // Status code 400 untuk Bad Request
-        return response;
   }
 }
 module.exports = storeData;

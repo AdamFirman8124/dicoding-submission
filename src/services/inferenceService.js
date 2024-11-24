cb156cfe8c2ecd196f14acce33eb5ee02a33a5b1
Masoyi <111947405+AdamFirman8124@users.noreply.github.com>
2024-11-25 01:02:55 +0700

    Update inferenceService.js

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -52,15 +52,6 @@ async function predictClassification(model, image) {
         return { confidenceScore, label, explanation, suggestion };
     } catch (error) {
      console.error(error); // Log error untuk debugging
-
-        // Mengembalikan respons error jika ada kesalahan
-        const response = h.response({
-            status: 'fail',
-            message: 'Terjadi kesalahan dalam melakukan prediksi',
-            error:error,
-        });
-        response.code(400); // Status code 400 untuk Bad Request
-        return response;
     }
 }
  
